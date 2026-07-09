a938e4a3ac7d1afa834c17920083f2d17c2fa017
VaishnaviN <154489462+Vaish2603@users.noreply.github.com>
2026-07-09 09:11:17 +0530

    postman Collection is added

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -11,3 +11,19 @@ router.put('/:id', teacherController.update);
 router.delete('/:id', teacherController.delete);
 
 module.exports = router;
+
+
+// teacherRoutes.js
+const express = require('express');
+const router = express.Router();
+const studentController = require('../controllers/studentController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+router.use(authMiddleware); // Protect all CRUD routes
+
+router.get('/', studentController.getAll);
+router.post('/', studentController.create);
+router.put('/:id', studentController.update);
+router.delete('/:id', studentController.delete);
+
+module.exports = router;
